b46a2e385ca03e60f4cd19d97487f0784eced200
methylDragon <methylDragon@gmail.com>
2021-03-23 04:22:34 +0800

    Implement more line methods

--- methyl-opencv-suite/mos/utils/line.py
+++ methyl-opencv-suite/mos/utils/line.py
@@ -27,11 +27,10 @@ class LinearLine:
     def __init__(self, points=None, m=None, c=None):
         assert points is None or (m is None and c is None)
 
-        if points:
+        if points is not None:
             self.points = np.array(points)
             self.m, self.c = LinearLine.linear_rect_params(points)
             self.rho, self.theta = LinearLine.linear_polar_params(points)
-
         else:
             self.m ,self.c = m, c
             self.points = np.array([
@@ -56,7 +55,6 @@ class LinearLine:
                 return points[0][0]
         except:
             pass
-
         return LinearLine.linear_resolve_x(y, self.m, self.c)
 
     def y_from_x(self, x):
@@ -67,8 +65,18 @@ class LinearLine:
         start, end = bounding_box
         m, c = self.m, self.c
 
+        ret = []
+
         if math.isinf(self.m): # Handle vertical lines
-            upper_y_intercept = lower_y_intercept = points[0][0]
+            upper_y_intercept = lower_y_intercept = self.points[0][0]
+
+            if upper_y_intercept <= end[0] and upper_y_intercept >= start[0]:
+                ret.append([upper_y_intercept, end[1]])
+
+            if lower_y_intercept <= end[0] and lower_y_intercept >= start[0]:
+                ret.append([lower_y_intercept, start[1]])
+
+            return np.array(ret)
         else:
             upper_y_intercept = LinearLine.linear_resolve_x(end[1], m, c)
             lower_y_intercept = LinearLine.linear_resolve_x(start[1], m, c)
@@ -76,7 +84,7 @@ class LinearLine:
         upper_x_intercept = LinearLine.linear_resolve_y(end[0], m, c)
         lower_x_intercept = LinearLine.linear_resolve_y(start[0], m, c)
 
-        ret = []
+        # Filter out of bounds
         if upper_y_intercept <= end[0] and upper_y_intercept >= start[0]:
             ret.append([upper_y_intercept, end[1]])
 
@@ -91,6 +99,16 @@ class LinearLine:
 
         return np.array(ret)
 
+    def normal_from_x(self, point, x):
+        with np.errstate(divide='ignore', invalid='ignore'):
+            normal_m = -1/self.m
+        return LinearLine.from_slope((x, self.y_from_x(x)), normal_m)
+
+    def normal_from_y(self, point, y):
+        with np.errstate(divide='ignore', invalid='ignore'):
+            normal_m = -1/self.m
+        return LinearLine.from_slope((self.x_from_y(y), y), normal_m)
+
     @classmethod
     def from_points(cls, points):
         return cls(points=points)
@@ -99,6 +117,21 @@ class LinearLine:
     def from_equation(cls, m, c):
         return cls(m=m, c=c)
 
+    @classmethod
+    def from_slope(cls, point, m):
+        return cls(points=np.array([point,
+                                    [point[0] + 1, point[1] + m]]))
+
+    @classmethod
+    def from_vector(cls, point, vector):
+        return cls(points=np.array(
+                [
+                    point,
+                    [point[0] + vector[0], point[1] + vector[1]]
+                ]
+            )
+        )
+
     @staticmethod
     def linear_resolve_x(y, m, c): # x = (y - c)/m
         with np.errstate(divide='ignore', invalid='ignore'):
@@ -106,7 +139,8 @@ class LinearLine:
 
     @staticmethod
     def linear_resolve_y(x, m, c): # y = mx + c
-        return m * x + c
+        with np.errstate(divide='ignore', invalid='ignore'):
+            return m * x + c
 
     @staticmethod
     def linear_slope(points): # Find m from two points
@@ -116,7 +150,8 @@ class LinearLine:
 
     @staticmethod
     def linear_intercept(point, m):
-        return point[1] - m * point[0]
+        with np.errstate(divide='ignore', invalid='ignore'):
+            return point[1] - m * point[0]
 
     @staticmethod
     def linear_rect_params(points):
@@ -157,5 +192,10 @@ def line_angle(line, degrees=False):
         return radians % math.pi
 
 def lines_to_angles(lines, degrees=False, dtype=np.float32):
-    angles = map(lambda x:line_angle(x, degrees=degrees), np.squeeze(lines))
+    if lines.shape != (1, 1, 4):
+        lines = np.squeeze(lines)
+    else:
+        lines = lines.reshape(1, 4)
+
+    angles = map(lambda x:line_angle(x, degrees=degrees), lines)
     return np.fromiter(angles, dtype=dtype).reshape(-1, 1)
