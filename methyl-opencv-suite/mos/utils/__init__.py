06d90b35d0425fe2208987027d8a41275e9af36b
methylDragon <methylDragon@gmail.com>
2021-03-22 21:53:57 +0800

    Implement meanshift clustering

--- methyl-opencv-suite/mos/utils/__init__.py
+++ methyl-opencv-suite/mos/utils/__init__.py
@@ -1,2 +1,4 @@
 from .utils import *
+from .line import *
+from .cluster import *
 from . import image_iterator
---
+++ methyl-opencv-suite/mos/utils/cluster.py
@@ -0,0 +1,27 @@
+from sklearn.cluster import MeanShift, estimate_bandwidth
+import numpy as np
+
+import math
+
+__all__ = [
+    'meanshift_cluster'
+]
+
+################################################################################
+# UTILITIES ====================================================================
+################################################################################
+def meanshift_cluster(data, bandwidth=None, bin_seeding=True, arg=False):
+    # https://scikit-learn.org/stable/modules/generated/sklearn.cluster.MeanShift.html
+    if bandwidth is None:
+        bandwidth = estimate_bandwidth(data)
+
+    ms = MeanShift(bandwidth=bandwidth, bin_seeding=bin_seeding)
+    ms.fit(data)
+
+    labels = ms.labels_
+    n_clusters = len(np.unique(labels))
+
+    if arg:
+        return [np.argwhere(labels == k).flatten() for k in range(n_clusters_)]
+    else:
+        return [data[labels == k, 0] for k in range(n_clusters_)]
