07f6b8b7d6350656dff5ce31a5bedf86e3ac9f91
methylDragon <methylDragon@gmail.com>
2021-03-24 03:16:22 +0800

    Implement MovingAverage class

--- methyl-opencv-suite/mos/utils/__init__.py
+++ methyl-opencv-suite/mos/utils/__init__.py
@@ -1,4 +1,6 @@
 from .utils import *
 from .line import *
 from .cluster import *
-from . import image_iterator
+from .classes.image_iterator import *
+from .classes.centroid_tracker import *
+from .classes.moving_average import *
---
+++ methyl-opencv-suite/mos/utils/classes/moving_average.py
@@ -0,0 +1,50 @@
+from collections import deque
+import numpy as np
+
+__all__ = [
+    'MovingAverage'
+]
+
+class MovingAverage:
+    """
+    Compute moving average with or without window.
+    """
+    def __init__(self,
+                 window=None,
+                 initial_entry=None):
+        self.window = window
+        self.init = False
+        self.entry_deque = None
+        self.average = None
+        self.data_shape = None
+
+        if initial_entry is not None:
+            initial_entry = np.array(initial_entry)
+            self._initialise(initial_entry)
+
+    def update(self, entry):
+        entry = np.array(entry)
+
+        if not self.init:
+            self._initialise(entry)
+
+        assert entry.shape == self.data_shape
+
+        oldest_entry = self.entry_deque[0]
+        self.average += (entry - oldest_entry) / self.window
+        self.entry_deque.append(entry)
+
+        return self.average
+
+    def _initialise(self, entry):
+        assert self.window is not None
+
+        if entry is not None:
+            self.data_shape = entry.shape
+
+            self.entry_deque = deque(np.zeros((self.window, *entry.shape)),
+                               maxlen=self.window)
+            self.entry_deque.appendleft(entry)
+
+            self.average = np.zeros(entry.shape)
+            self.init = True
